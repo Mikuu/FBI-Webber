3b4383e053c00c72df05355d3dfe79777dcfc2e9
mikuu <biaofu@thoughtworksc.om>
2017-08-28 10:21:54 +0800

    update downloader

--- API/run.sh
+++ API/run.sh
@@ -1,3 +1,1 @@
-python tools/get-wiremock.py
-
 java -jar tools/wiremock-standalone-2.7.1.jar --root-dir=12306 --port=8080
--- API/tools/get-wiremock.py
+++ API/tools/get-wiremock.py
@@ -1,9 +1,11 @@
 import os, shutil
+from urllib import urlretrieve
 
 wm_version = '2.7.1'
 wm_binary = 'wiremock-standalone-{0}.jar'.format(wm_version)
-wm_location = os.path.join('tools', wm_binary)
 wm_url = 'http://repo1.maven.org/maven2/com/github/tomakehurst/wiremock-standalone/{0}/{1}'.format(wm_version, wm_binary)
 
-if not os.path.isfile(wm_location):
-  os.system('wget -P tools {0}'.format(wm_url))
+if not os.path.isfile(wm_binary):
+  print 'Download {0} ...'.format(wm_binary)
+  urlretrieve(wm_url, filename=wm_binary)
+  print '... Done'
