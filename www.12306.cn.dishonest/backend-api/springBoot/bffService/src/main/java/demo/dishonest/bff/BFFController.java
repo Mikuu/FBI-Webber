ec01ee1d834f6beafd6800cf5b776599be7192de
mikuu <biao.fu@thoughtworks.com>
2019-05-25 13:37:11 +0800

    update 12306.cn.dishonest

--- www.12306.cn.dishonest/backend-api/springBoot/bffService/src/main/java/demo/dishonest/bff/BFFController.java
+++ www.12306.cn.dishonest/backend-api/springBoot/bffService/src/main/java/demo/dishonest/bff/BFFController.java
@@ -2,9 +2,11 @@ package demo.dishonest.bff;
 
 import demo.dishonest.bff.represent.Represent;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin
 @RestController
 public class BFFController {
 
